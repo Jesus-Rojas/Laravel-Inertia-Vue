9171634dcb5ba537c411a2e88257d2ee7f41b0fb
Jesus Rojas <jarojas6524@misena.edu.co>
2022-03-08 06:20:20 -0500

    config app-service test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,8 +21,8 @@ class AppServiceProvider extends ServiceProvider
                 'status' => Session::get('status')
             ];
         });
-        // if(env('APP_ENV') == 'production'){
-        //     URL::forceScheme('https');
-        // }
+        if(env('APP_ENV') == 'production'){
+            URL::forceScheme('https');
+        }
     }
 }
